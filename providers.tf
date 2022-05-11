8c787a0ca8bf8e57d1ec9459c9a905ec0b49c7d0
yashu1506 <98607047+yashu1506@users.noreply.github.com>
2022-05-11 16:24:30 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = "1.1.9"
-  backend "s3" {}
+  backend "s3" {
+    bucket = "terraformdemo-1"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+  }
 
   required_providers {
     aws = {
